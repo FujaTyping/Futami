34fbade2c90a505bb23f0d2763955cf81d24a0f7
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2023-09-30 22:28:02 +0700

    Add status command

---
+++ src/commands/status.js
@@ -0,0 +1,49 @@
+const { Command, container } = require('@sapphire/framework');
+const { EmbedBuilder, ActivityType } = require('discord.js');
+
+const { color, owner } = require('../config.json');
+
+class StatusCommand extends Command {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            name: 'status',
+            aliases: ['st'],
+            description: 'change bot status'
+        });
+    }
+
+    async messageRun(message, args) {
+        const { client } = container;
+        if (message.author.id == owner) {
+            const Status = await args.rest('string');
+
+            client.user.setPresence({
+                activities: [{
+                    name: Status,
+                    type: ActivityType.Streaming,
+                    url: "https://www.twitch.tv/anime"
+                }]
+            });
+
+            const Content = new EmbedBuilder()
+                .setColor(color)
+                .setTitle(`อัพเดทสถานะแล้ว ✅`)
+                .setDescription(`ชื่อสถานะ : **` + Status + '**\nหมวดหมู่ : `Streaming`')
+                .setTimestamp()
+
+            return message.reply({ embeds: [Content] });
+        } else {
+            const Content = new EmbedBuilder()
+                .setColor(color)
+                .setTitle('⚠️ เตือน !!')
+                .setDescription('เป็นผู้พัฒนาถึงใช้งานคำสั่งได้')
+                .setTimestamp()
+
+            return message.reply({ embeds: [Content] });
+        }
+    }
+}
+module.exports = {
+    StatusCommand
+};
