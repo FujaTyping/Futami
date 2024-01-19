87e3f0127576f1849854c71e4c9198881dbaeb09
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-01-19 19:02:45 +0700

    Delete old deps , Random activities presence

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -3,13 +3,19 @@ const { ActivityType } = require('discord.js');
 
 class ReadyListener extends Listener {
     run(client) {
-        client.user.setPresence({
-            activities: [{
-                name: `in Voice Channel`,
-                type: ActivityType.Streaming,
-                url: "https://www.twitch.tv/anime"
-            }]
-        });
+        const ActivityText = [`in ${client.guilds.cache.size} Servers`, 'at Voice Channel', 'with You']
+
+        setInterval(() => {
+            const RandomActivityText = Math.floor(Math.random() * ActivityText.length);
+
+            client.user.setPresence({
+                activities: [{
+                    name: ActivityText[RandomActivityText],
+                    type: ActivityType.Streaming,
+                    url: "https://www.twitch.tv/anime"
+                }]
+            });
+        }, 15000)
     }
 }
 module.exports = {
