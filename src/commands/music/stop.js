26b459fa4ebe9b4d9cc0a8ce29e70191f205e591
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-04-19 00:44:52 +0700

    Add donation

--- src/commands/music/stop.js
+++ src/commands/music/stop.js
@@ -1,5 +1,5 @@
 const { Command, container } = require('@sapphire/framework');
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 const { color } = require('../../config.json');
 
@@ -27,10 +27,18 @@ class StopCommand extends Command {
         } else {
             client.distube.voices.leave(message)
 
+            const Button = new ButtonBuilder()
+                .setLabel('สนับสนุนฟูตามิโดยการชื้อกาแฟให้ได้นะ')
+                .setURL('https://buymeacoffee.com/fujatyping')
+                .setStyle(ButtonStyle.Link);
+
+            const Row = new ActionRowBuilder()
+                .addComponents(Button);
+
             const Content = new EmbedBuilder()
                 .setColor(color)
                 .setTitle('👋🏻 ออกจากห้องแล้ว !!')
-                .setDescription('ไว้เจอกันใหม่น้า ~~\nขอบคุณที่ใช้ ฟูตามิ 💙')
+                .setDescription('ขอบคุณที่ใช้ ฟูตามิ 💙 ไว้เจอกันใหม่น้า ~~\nสนับสนุนฟูตามิโดยการซื้อกาแฟได้ที่ : <:BTBMAC:1230573925881221162> [Buy me a coffee](https://buymeacoffee.com/fujatyping)')
                 .setFooter({ text: `ใช้คำสั่งโดย : ${message.author.username}`, iconURL: message.author.avatarURL() })
                 .setTimestamp()
 
