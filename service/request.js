1abc77dd5c8d2dfda89cec31dea588e040eb56c1
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-04-27 20:56:40 +0700

    Deploy

--- service/request.js
+++ service/request.js
@@ -1,4 +1,50 @@
-window.onload = GetLastData()
+window.onload = CheckPath()
+
+function CheckPath() {
+    let CurrentUrl = window.location.pathname;
+    //console.log(CurrentUrl)
+
+    if (CurrentUrl.includes("/statics") || CurrentUrl.includes("/statics.html")) {
+        GetLastData();
+    } else if (CurrentUrl.includes("/") || CurrentUrl.includes("/index.html") || CurrentUrl.includes("/index")) {
+        GetStat();
+    }
+}
+
+function GetStat() {
+    axios.get('https://api.futami.siraphop.me:6947/system')
+        .then(function (response) {
+            const BotData = response.data.Bot
+            const Guild = BotData.guild
+            const Users = BotData.users
+
+            const DataGuild = document.getElementById('DataGuild');
+            const DataUsers = document.getElementById('DataUsers');
+            const Loader1 = document.getElementById('Loader1');
+            const Loader2 = document.getElementById('Loader2');
+
+            DataGuild.textContent = Guild
+            DataUsers.textContent = Users
+            Loader1.style.display = 'none';
+            Loader2.style.display = 'none';
+            DataGuild.style.display = 'block';
+            DataUsers.style.display = 'block';
+        })
+        .catch(function (error) {
+            console.error('[ERROR] : ', error);
+
+            const ErrorLoader1 = document.getElementById('LoaderError1')
+            const ErrorLoader2 = document.getElementById('LoaderError2')
+            const Loader1 = document.getElementById('Loader1');
+            const Loader2 = document.getElementById('Loader2');
+
+            Loader1.style.display = 'none';
+            Loader2.style.display = 'none';
+            ErrorLoader1.style.display = 'block';
+            ErrorLoader2.style.display = 'block';
+        });
+
+}
 
 function GetLastData() {
     const Loader = document.getElementById('FetchLoader')
@@ -64,6 +110,15 @@ function GetLastData() {
             console.error('[ERROR] : ', error);
         });
 
+    if (FailedCount >= 1) {
+        AlertFail.style.display = 'flex';
+    } else {
+        Loader.style.display = 'none';
+        FTable.style.display = 'block';
+        ContainF.classList.remove('min-h-screen');
+        //ContainF.classList.add('sas-content');
+    }
+    /*
     setTimeout(function () {
         if (FailedCount >= 1) {
             AlertFail.style.display = 'flex';
@@ -74,4 +129,5 @@ function GetLastData() {
             //ContainF.classList.add('sas-content');
         }
     }, Math.floor(Math.random() * 3001) + 2000);
+    */
 }
