15361b1c5ff59117850ae50e93f965b6a0055924
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-05-21 20:02:17 +0700

    Fix welcome / goodmessage only 1 guild

--- src/listeners/guildMemberRemove.js
+++ src/listeners/guildMemberRemove.js
@@ -5,48 +5,51 @@ const { registerFont, createCanvas, loadImage } = require('canvas')
 const config = require('../config.json');
 const color = config.chat.color
 const system = config.bot.systemchannel
+const guildID = config.bot.guild
 const compact = true
 
 //registerFont('./src/assets/canvas/font/UniSans.otf', { family: 'Uni Sans' })
 
 class GuildMemberRemoveListener extends Listener {
     run(member) {
-        const Channel = member.guild.channels.cache.get(system)
+        if (member.guild.id == guildID) {
+            const Channel = member.guild.channels.cache.get(system)
 
-        const canvas = createCanvas(1024, 500)
-        const ctx = canvas.getContext('2d')
-        const Text = member.user.username
-        loadImage('./src/assets/canvas/GoodBye.png').then((Bg) => {
-            loadImage(`${member.user.avatarURL().replace(".webp", ".png")}`).then((Avatar) => {
-                ctx.drawImage(Bg, 0, 0, canvas.width, canvas.height)
-                ctx.drawImage(Avatar, 138.5, 42, 165, 165)
+            const canvas = createCanvas(1024, 500)
+            const ctx = canvas.getContext('2d')
+            const Text = member.user.username
+            loadImage('./src/assets/canvas/GoodBye.png').then((Bg) => {
+                loadImage(`${member.user.avatarURL().replace(".webp", ".png")}`).then((Avatar) => {
+                    ctx.drawImage(Bg, 0, 0, canvas.width, canvas.height)
+                    ctx.drawImage(Avatar, 138.5, 42, 165, 165)
 
-                const x = 360;
-                const y = 452;
+                    const x = 360;
+                    const y = 452;
 
-                ctx.font = '40px Uni Sans'
-                ctx.fillStyle = 'white'
-                ctx.textAlign = 'center'
-                ctx.fillText(Text, x, y)
+                    ctx.font = '40px Uni Sans'
+                    ctx.fillStyle = 'white'
+                    ctx.textAlign = 'center'
+                    ctx.fillText(Text, x, y)
 
-                const Buffer = canvas.toBuffer('image/png')
-                const FinishImage = new AttachmentBuilder(Buffer, { name: 'GoodBye.png' });
+                    const Buffer = canvas.toBuffer('image/png')
+                    const FinishImage = new AttachmentBuilder(Buffer, { name: 'GoodBye.png' });
 
-                if (compact) {
-                    return Channel.send({ files: [FinishImage] })
-                } else {
-                    const Content = new EmbedBuilder()
-                        .setColor(7054307)
-                        .setTitle(`สมาชิกได้ออกไปแล้ว 👋🏻`)
-                        .setDescription(`<@${member.id}> ได้ออกจาก **${member.guild.name}** แล้ว !\nสมาชิกที่เหลือทั้งหมด **${member.guild.memberCount}** คนของดิสนี้`)
-                        .setImage('attachment://GoodBye.png')
-                        .setFooter({ text: `ข้อความอัตโนมัติ` })
-                        .setTimestamp()
+                    if (compact) {
+                        return Channel.send({ files: [FinishImage] })
+                    } else {
+                        const Content = new EmbedBuilder()
+                            .setColor(7054307)
+                            .setTitle(`สมาชิกได้ออกไปแล้ว 👋🏻`)
+                            .setDescription(`<@${member.id}> ได้ออกจาก **${member.guild.name}** แล้ว !\nสมาชิกที่เหลือทั้งหมด **${member.guild.memberCount}** คนของดิสนี้`)
+                            .setImage('attachment://GoodBye.png')
+                            .setFooter({ text: `ข้อความอัตโนมัติ` })
+                            .setTimestamp()
 
-                    return Channel.send({ embeds: [Content], files: [FinishImage] })
-                }
+                        return Channel.send({ embeds: [Content], files: [FinishImage] })
+                    }
+                })
             })
-        })
+        }
     }
 }
 module.exports = {
