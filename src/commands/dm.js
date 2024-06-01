b339a149ffb848592ecf50e19dc961c106e7ef57
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-06-01 13:00:17 +0700

    Update dm.js

--- src/commands/dm.js
+++ src/commands/dm.js
@@ -21,15 +21,26 @@ class DMCommand extends Command {
         const { client } = container;
         const Message = await args.rest('string');
         const Args = Message.split(' ');
-        client.users.send(Args[0], `${Args.slice(1).join(' ')}`);
 
-        const Content = new EmbedBuilder()
-            .setColor(color)
-            .setAuthor({ name: 'ส่งข้อความไปยังส่วนตัวเรียบร้อยแล้ว !!', iconURL: 'https://futami.siraphop.me/assets/icon/checked.png' })
-            .setDescription(`✉️ ข้อความ : **${Args.slice(1).join(' ')}**\n📩 ส่งไปยัง : **<@${Args[0]}>**`)
-            .setTimestamp()
+        if (Args[0] == client.user.id) {
+            const Content = new EmbedBuilder()
+                .setColor(color)
+                .setAuthor({ name: 'เตือน !!', iconURL: 'https://futami.siraphop.me/assets/icon/warning.png' })
+                .setDescription(`ไม่สามารถส่งข้อความถึงตัวบอทเองได้`)
+                .setTimestamp()
 
-        return message.reply({ embeds: [Content] });
+            return message.reply({ embeds: [Content] });
+        } else {
+            client.users.send(Args[0], `${Args.slice(1).join(' ')}`);
+
+            const Content = new EmbedBuilder()
+                .setColor(color)
+                .setAuthor({ name: 'ส่งข้อความไปยังส่วนตัวเรียบร้อยแล้ว !!', iconURL: 'https://futami.siraphop.me/assets/icon/checked.png' })
+                .setDescription(`✉️ ข้อความ : **${Args.slice(1).join(' ')}**\n📩 ส่งไปยัง : **<@${Args[0]}>**`)
+                .setTimestamp()
+
+            return message.reply({ embeds: [Content] });
+        }
     }
 }
 module.exports = {
