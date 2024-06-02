1000262fbe346c0e031c33eaed8dcf07a4b921da
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-06-02 11:11:43 +0700

    Use napi-rs/canvas instaed of node-canvas

--- src/listeners/guildMemberAdd.js
+++ src/listeners/guildMemberAdd.js
@@ -1,6 +1,6 @@
 const { Listener } = require('@sapphire/framework');
 const { AttachmentBuilder, EmbedBuilder } = require('discord.js');
-const { registerFont, createCanvas, loadImage } = require('canvas')
+const { GlobalFonts, createCanvas, loadImage } = require('@napi-rs/canvas')
 
 const config = require('../config.json');
 const color = config.chat.color
@@ -8,47 +8,47 @@ const system = config.bot.systemchannel
 const guildID = config.bot.guild
 const compact = true
 
-//registerFont('./src/assets/canvas/font/UniSans.otf', { family: 'Uni Sans' })
+GlobalFonts.registerFromPath('./src/assets/canvas/font/UniSans.otf', 'Uni Sans')
 
 class GuildMemberAddListener extends Listener {
-    run(member) {
+    async run(member) {
         if (member.guild.id == guildID) {
             const Channel = member.guild.channels.cache.get(system)
 
             const canvas = createCanvas(1024, 500)
             const ctx = canvas.getContext('2d')
             const Text = member.user.username
-            loadImage('./src/assets/canvas/Welcome.png').then((Bg) => {
-                loadImage(`${member.user.avatarURL().replace(".webp", ".png")}`).then((Avatar) => {
-                    ctx.drawImage(Bg, 0, 0, canvas.width, canvas.height)
-                    ctx.drawImage(Avatar, 453.5, 42, 165, 165)
 
-                    const x = 660;
-                    const y = 452;
+            const Bg = await loadImage('./src/assets/canvas/Welcome.png')
+            const Avatar = await loadImage(`${member.user.avatarURL().replace(".webp", ".png")}`)
 
-                    ctx.font = '40px Uni Sans'
-                    ctx.fillStyle = 'white'
-                    ctx.textAlign = 'center'
-                    ctx.fillText(Text, x, y)
+            ctx.drawImage(Bg, 0, 0, canvas.width, canvas.height)
+            ctx.drawImage(Avatar, 453.5, 42, 165, 165)
 
-                    const Buffer = canvas.toBuffer('image/png')
-                    const FinishImage = new AttachmentBuilder(Buffer, { name: 'Welcome.png' });
+            const x = 660;
+            const y = 452;
 
-                    if (compact) {
-                        return Channel.send({ files: [FinishImage] })
-                    } else {
-                        const Content = new EmbedBuilder()
-                            .setColor(16195357)
-                            .setTitle(`🎉 ยินดีต้อนรับ สมาชิกใหม่ !`)
-                            .setDescription(`ยินดีต้อนรับ <@${member.id}> เข้าสู่ **${member.guild.name}** !\nคุณเป็นสมาชิกคนที่ **#${member.guild.memberCount}** ของดิสนี้`)
-                            .setImage('attachment://Welcome.png')
-                            .setFooter({ text: `ข้อความอัตโนมัติ` })
-                            .setTimestamp()
+            ctx.font = '40px Uni Sans'
+            ctx.fillStyle = 'white'
+            ctx.textAlign = 'center'
+            ctx.fillText(Text, x, y)
 
-                        return Channel.send({ embeds: [Content], files: [FinishImage] })
-                    }
-                })
-            })
+            const Buffer = canvas.toBuffer('image/png')
+            const FinishImage = new AttachmentBuilder(Buffer, { name: 'Welcome.png' });
+
+            if (compact) {
+                return Channel.send({ files: [FinishImage] })
+            } else {
+                const Content = new EmbedBuilder()
+                    .setColor(16195357)
+                    .setTitle(`🎉 ยินดีต้อนรับ สมาชิกใหม่ !`)
+                    .setDescription(`ยินดีต้อนรับ <@${member.id}> เข้าสู่ **${member.guild.name}** !\nคุณเป็นสมาชิกคนที่ **#${member.guild.memberCount}** ของดิสนี้`)
+                    .setImage('attachment://Welcome.png')
+                    .setFooter({ text: `ข้อความอัตโนมัติ` })
+                    .setTimestamp()
+
+                return Channel.send({ embeds: [Content], files: [FinishImage] })
+            }
         }
     }
 }
--- src/listeners/guildMemberRemove.js
+++ src/listeners/guildMemberRemove.js
@@ -1,6 +1,6 @@
 const { Listener } = require('@sapphire/framework');
 const { AttachmentBuilder, EmbedBuilder } = require('discord.js');
-const { registerFont, createCanvas, loadImage } = require('canvas')
+const { GlobalFonts, createCanvas, loadImage } = require('@napi-rs/canvas')
 
 const config = require('../config.json');
 const color = config.chat.color
@@ -8,47 +8,47 @@ const system = config.bot.systemchannel
 const guildID = config.bot.guild
 const compact = true
 
-//registerFont('./src/assets/canvas/font/UniSans.otf', { family: 'Uni Sans' })
+GlobalFonts.registerFromPath('./src/assets/canvas/font/UniSans.otf', 'Uni Sans')
 
 class GuildMemberRemoveListener extends Listener {
-    run(member) {
+    async run(member) {
         if (member.guild.id == guildID) {
             const Channel = member.guild.channels.cache.get(system)
 
             const canvas = createCanvas(1024, 500)
             const ctx = canvas.getContext('2d')
             const Text = member.user.username
-            loadImage('./src/assets/canvas/GoodByeREM.png').then((Bg) => {
-                loadImage(`${member.user.avatarURL().replace(".webp", ".png")}`).then((Avatar) => {
-                    ctx.drawImage(Bg, 0, 0, canvas.width, canvas.height)
-                    ctx.drawImage(Avatar, 407, 42, 165, 165)
 
-                    const x = 360;
-                    const y = 452;
+            const Bg = await loadImage('./src/assets/canvas/GoodByeREM.png')
+            const Avatar = await loadImage(`${member.user.avatarURL().replace(".webp", ".png")}`)
 
-                    ctx.font = '40px Uni Sans'
-                    ctx.fillStyle = 'white'
-                    ctx.textAlign = 'center'
-                    ctx.fillText(Text, x, y)
+            ctx.drawImage(Bg, 0, 0, canvas.width, canvas.height)
+            ctx.drawImage(Avatar, 407, 42, 165, 165)
 
-                    const Buffer = canvas.toBuffer('image/png')
-                    const FinishImage = new AttachmentBuilder(Buffer, { name: 'GoodBye.png' });
+            const x = 360;
+            const y = 452;
 
-                    if (compact) {
-                        return Channel.send({ files: [FinishImage] })
-                    } else {
-                        const Content = new EmbedBuilder()
-                            .setColor(7054307)
-                            .setTitle(`สมาชิกได้ออกไปแล้ว 👋🏻`)
-                            .setDescription(`<@${member.id}> ได้ออกจาก **${member.guild.name}** แล้ว !\nสมาชิกที่เหลือทั้งหมด **${member.guild.memberCount}** คนของดิสนี้`)
-                            .setImage('attachment://GoodBye.png')
-                            .setFooter({ text: `ข้อความอัตโนมัติ` })
-                            .setTimestamp()
+            ctx.font = '40px Uni Sans'
+            ctx.fillStyle = 'white'
+            ctx.textAlign = 'center'
+            ctx.fillText(Text, x, y)
 
-                        return Channel.send({ embeds: [Content], files: [FinishImage] })
-                    }
-                })
-            })
+            const Buffer = canvas.toBuffer('image/png')
+            const FinishImage = new AttachmentBuilder(Buffer, { name: 'GoodBye.png' });
+
+            if (compact) {
+                return Channel.send({ files: [FinishImage] })
+            } else {
+                const Content = new EmbedBuilder()
+                    .setColor(7054307)
+                    .setTitle(`สมาชิกได้ออกไปแล้ว 👋🏻`)
+                    .setDescription(`<@${member.id}> ได้ออกจาก **${member.guild.name}** แล้ว !\nสมาชิกที่เหลือทั้งหมด **${member.guild.memberCount}** คนของดิสนี้`)
+                    .setImage('attachment://GoodBye.png')
+                    .setFooter({ text: `ข้อความอัตโนมัติ` })
+                    .setTimestamp()
+
+                return Channel.send({ embeds: [Content], files: [FinishImage] })
+            }
         }
     }
 }
