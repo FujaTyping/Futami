d5caeea941b86d2b42a5e2dfda702a3858c8e63f
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-06-03 11:44:03 +0700

    Add Request modal

---
+++ src/commands/request.js
@@ -0,0 +1,46 @@
+const { isMessageInstance } = require('@sapphire/discord.js-utilities');
+const { Command } = require('@sapphire/framework');
+const { EmbedBuilder, ActionRowBuilder, Events, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+
+const config = require('../config.json');
+const color = config.chat.color
+
+class RequestCommand extends Command {
+    constructor(context, options) {
+        super(context, { ...options });
+    }
+
+    registerApplicationCommands(registry) {
+        registry.registerChatInputCommand((builder) =>
+            builder.setName('request').setDescription('อยากขออะไรฟูตามิหละ ??')
+        );
+    }
+
+    async chatInputRun(interaction) {
+        const Modal = new ModalBuilder()
+            .setCustomId('RequestModal')
+            .setTitle('ฟูตามิ | ความคิดเห็น');
+
+        const FutamiScoreInput = new TextInputBuilder()
+            .setCustomId('FutamiscoreInput')
+            .setMaxLength(50)
+            .setLabel("คิดเห็นยังไงกับฟูตามิ ตอนนี้ ??")
+            .setStyle(TextInputStyle.Short);
+
+        const FutamiRecomendInput = new TextInputBuilder()
+            .setCustomId('FutamirecomendInput')
+            .setMaxLength(200)
+            .setLabel("อยากให้ฟูตามิเพิ่มอะไรเข้ามา บอกได้เลยนะ !!")
+            .setStyle(TextInputStyle.Paragraph);
+
+        const FirstActionRow = new ActionRowBuilder().addComponents(FutamiScoreInput);
+        const SecondActionRow = new ActionRowBuilder().addComponents(FutamiRecomendInput);
+
+        Modal.addComponents(FirstActionRow, SecondActionRow);
+
+        await interaction.showModal(Modal);
+    }
+}
+module.exports = {
+    RequestCommand
+};
---
+++ src/interaction-handlers/RequestModals.js
@@ -0,0 +1,49 @@
+const { InteractionHandler, InteractionHandlerTypes, container } = require('@sapphire/framework');
+const { EmbedBuilder } = require('discord.js');
+const config = require('../config.json');
+
+const color = config.chat.color
+
+class RequestModalHandler extends InteractionHandler {
+    constructor(ctx, options) {
+        super(ctx, {
+            ...options,
+            interactionHandlerType: InteractionHandlerTypes.ModalSubmit
+        });
+    }
+
+    parse(interaction) {
+        if (interaction.customId !== 'RequestModal') return this.none();
+
+        return this.some();
+    }
+
+    async run(interaction) {
+        const { client } = container;
+        const Channel = client.channels.cache.get('1247044218657046589');
+
+        const Content = new EmbedBuilder()
+            .setColor(color)
+            .setAuthor({ name: 'ส่งแบบฟอร์มเรียบร้อยแล้ว !!', iconURL: 'https://futami.siraphop.me/assets/icon/checked.png' })
+            .setDescription('ขอบคุณที่ให้คำแนะนำฟูตามิ , เราจะพัฒนาฟูตามิให้ดียิ่งขั้น 🩵')
+            .setTimestamp()
+
+        await interaction.reply({ embeds: [Content] });
+
+        const FormContent = new EmbedBuilder()
+            .setColor(color)
+            .setTitle('📥 แบบฟอร์มคำแนะนำฟูตามิ')
+            .setDescription(`**✏️ กรอกโดย** : <@${interaction.user.id}> (${interaction.user.username})`)
+            .addFields(
+                { name: '**คิดเห็นยังไงกับฟูตามิ ตอนนี้ ??**', value: `${interaction.fields.getTextInputValue('FutamiscoreInput')}` },
+                { name: '**อยากให้ฟูตามิเพิ่มอะไรเข้ามา บอกได้เลยนะ !!**', value: `${interaction.fields.getTextInputValue('FutamirecomendInput')}` }
+            )
+            .setFooter({ text: 'ความคิดเห็นจากผู้ใช้' })
+            .setTimestamp()
+
+        Channel.send({ embeds: [FormContent] });
+    }
+}
+module.exports = {
+    RequestModalHandler
+};
