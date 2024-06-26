9c7e105341d7990835a831096f2dc70f2888a2eb
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-06-26 19:26:46 +0700

    Fix 404 ; Dismiss type and add 'Trap' choice

--- src/commands/fun/waifu.js
+++ src/commands/fun/waifu.js
@@ -7,6 +7,13 @@ const config = require('../../config.json');
 const color = config.chat.color
 const emote = config.default
 
+const Choices = [
+    { name: 'Waifu', value: 'waifu' },
+    { name: 'Neko', value: 'neko' },
+    { name: 'Megumin', value: 'megumin' },
+    { name: 'Shinobu', value: 'shinobu' },
+    { name: 'Trap', value: 'trap' }
+];
 class WaifuCommand extends Command {
     constructor(context, options) {
         super(context, { ...options });
@@ -23,7 +30,8 @@ class WaifuCommand extends Command {
                             { name: 'Waifu', value: 'waifu' },
                             { name: 'Neko', value: 'neko' },
                             { name: 'Megumin', value: 'megumin' },
-                            { name: 'Shinobu', value: 'shinobu' }
+                            { name: 'Shinobu', value: 'shinobu' },
+                            { name: 'Trap', value: 'trap' }
                         )
                         .setRequired(true)
                 )
@@ -35,51 +43,28 @@ class WaifuCommand extends Command {
                             { name: 'SFW', value: 'sfw' },
                             { name: 'NSFW', value: 'nsfw' }
                         )
-                        .setRequired(true)
                 )
         );
     }
 
     async chatInputRun(interaction) {
-        const Type = interaction.options.getString('type')
-        const Mode = interaction.options.getString('mode')
+        const Type = interaction.options.getString('type');
+        const Mode = interaction.options.getString('mode') ?? 'sfw';
+        const NameType = Choices.find(choice => choice.value === Type)?.name;
 
         if (Mode == 'sfw') {
-            const Content = new EmbedBuilder()
-                .setColor(color)
-                .setTitle('😇 คลังรูปภาพ Waifu')
-                .setDescription('กำลังสุ่มรูปภาพ ..')
-                .setTimestamp()
-
-            const msg = await interaction.reply({ embeds: [Content], fetchReply: true });
-
-            axios.get(`https://api.waifu.pics/${Mode}/${Type}`)
-                .then(response => {
-                    const Response = response.data;
-
-                    const Content = new EmbedBuilder()
-                        .setColor(color)
-                        .setTitle('😇 คลังรูปภาพ Waifu')
-                        .setDescription('ไม่ต้องห่วงภาพนี้ยังคงเป็น **SFW** 🙃')
-                        .setImage(`${Response.url}`)
-                        .setTimestamp()
-
-                    return interaction.editReply({ embeds: [Content] });
-                })
-                .catch(error => {
-                    const Content = new EmbedBuilder()
-                        .setColor(color)
-                        .setAuthor({ name: 'เกิดอะไรขึ้น ??', iconURL: 'https://futami.siraphop.me/assets/icon/error.png' })
-                        .setDescription("```\n" + error + "\n```")
-                        .setTimestamp()
-
-                    return interaction.editReply({ embeds: [Content] });
-                });
-        } else {
-            if (interaction.channel.nsfw) {
+            if (Type == 'trap') {
                 const Content = new EmbedBuilder()
                     .setColor(color)
-                    .setTitle('🔞 คลังรูปภาพ Waifu')
+                    .setAuthor({ name: 'เตือน !!', iconURL: 'https://futami.siraphop.me/assets/icon/warning.png' })
+                    .setDescription(`รูปแบบ **${NameType}** มีแค่ใน **NSFW** เท่านั้น`)
+                    .setTimestamp()
+
+                return interaction.reply({ embeds: [Content] });
+            } else {
+                const Content = new EmbedBuilder()
+                    .setColor(color)
+                    .setTitle('😇 คลังรูปภาพ Waifu')
                     .setDescription('กำลังสุ่มรูปภาพ ..')
                     .setTimestamp()
 
@@ -91,8 +76,8 @@ class WaifuCommand extends Command {
 
                         const Content = new EmbedBuilder()
                             .setColor(color)
-                            .setTitle('🔞 คลังรูปภาพ Waifu')
-                            .setDescription('ระวังด้วยหละภาพนี้เป็น **NSFW**')
+                            .setTitle('😇 คลังรูปภาพ Waifu')
+                            .setDescription('ไม่ต้องห่วงภาพนี้ยังคงเป็น **SFW** 🙃')
                             .setImage(`${Response.url}`)
                             .setTimestamp()
 
@@ -107,14 +92,57 @@ class WaifuCommand extends Command {
 
                         return interaction.editReply({ embeds: [Content] });
                     });
-            } else {
+            }
+        } else {
+            if (Type == 'megumin' || Type == 'shinobu') {
                 const Content = new EmbedBuilder()
                     .setColor(color)
                     .setAuthor({ name: 'เตือน !!', iconURL: 'https://futami.siraphop.me/assets/icon/warning.png' })
-                    .setDescription(`ใช้คำสั่งนี้ด้วยโหมด **NSFW** ต้องใช้ในช่องที่จำกัดอายุเท่านั้น`)
+                    .setDescription(`รูปแบบ **${NameType}** มีแค่ใน **SFW** เท่านั้น`)
                     .setTimestamp()
 
                 return interaction.reply({ embeds: [Content] });
+            } else {
+                if (interaction.channel.nsfw) {
+                    const Content = new EmbedBuilder()
+                        .setColor(color)
+                        .setTitle('🔞 คลังรูปภาพ Waifu')
+                        .setDescription('กำลังสุ่มรูปภาพ ..')
+                        .setTimestamp()
+
+                    const msg = await interaction.reply({ embeds: [Content], fetchReply: true });
+
+                    axios.get(`https://api.waifu.pics/${Mode}/${Type}`)
+                        .then(response => {
+                            const Response = response.data;
+
+                            const Content = new EmbedBuilder()
+                                .setColor(color)
+                                .setTitle('🔞 คลังรูปภาพ Waifu')
+                                .setDescription('ระวังด้วยหละภาพนี้เป็น **NSFW**')
+                                .setImage(`${Response.url}`)
+                                .setTimestamp()
+
+                            return interaction.editReply({ embeds: [Content] });
+                        })
+                        .catch(error => {
+                            const Content = new EmbedBuilder()
+                                .setColor(color)
+                                .setAuthor({ name: 'เกิดอะไรขึ้น ??', iconURL: 'https://futami.siraphop.me/assets/icon/error.png' })
+                                .setDescription("```\n" + error + "\n```")
+                                .setTimestamp()
+
+                            return interaction.editReply({ embeds: [Content] });
+                        });
+                } else {
+                    const Content = new EmbedBuilder()
+                        .setColor(color)
+                        .setAuthor({ name: 'เตือน !!', iconURL: 'https://futami.siraphop.me/assets/icon/warning.png' })
+                        .setDescription(`ใช้คำสั่งนี้ด้วยโหมด **NSFW** ต้องใช้ในช่องที่จำกัดอายุเท่านั้น`)
+                        .setTimestamp()
+
+                    return interaction.reply({ embeds: [Content] });
+                }
             }
         }
     }
